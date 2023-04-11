9d31fe74c8ec72377d28e6b8976b38a907020b30
lucas lira <lucas.lira.silva98@gmail.com>
2023-04-10 21:01:23 -0300

    add gitignore file

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
-print('hello world')
+# test_refactor = 10
+# print(f"x vale: {test_refactor}")
+#
+# print(test_refactor * 2)
+# print(test_refactor * 3)
+# print(test_refactor * 4)
+
+x = 5
+#gerado por ctrl + alt + t
+while True:
+    x
